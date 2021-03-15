0ebba6752084bd8e3aff015e3b1385b605e170f5
IASADMIN\william.crowe <william.crowe@iasadmin.com>
2021-03-15 16:35:25 -0400

    Dapper Paramater Attributes

---
+++ DapperStoredProcAttributes/DapperExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalData.Utils
+{
+
+    static public class DapperExtensions
+    {
+
+        public static MemberInfo GetMemberInfo(this Expression expression)
+        {
+            LambdaExpression lambdaExpression = (LambdaExpression)expression;
+            MemberExpression memberExpression;
+            if (lambdaExpression.Body is UnaryExpression)
+            {
+                UnaryExpression unaryExpression = (UnaryExpression)lambdaExpression.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambdaExpression.Body;
+            }
+            return memberExpression.Member;
+        }
+
+        public static TReturn GetValue<TReturn, TProperty>(this DynamicParameters dynamicParameters, Expression<Func<TProperty>> property)
+        {
+            return dynamicParameters.Get<TReturn>("@" + property.GetMemberInfo().Name);
+        }
+
+
+        //Feel free to use this as you want 
+        public static DynamicParameters PrepareParams<T>(this IDbConnection connection, T entity) where T : class
+        {
+            var dynamicParameters = new DynamicParameters();
+            var type = typeof(T);
+            var props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                ParameterTypeAttribute[] attributes = (ParameterTypeAttribute[])prop.GetCustomAttributes(
+               typeof(ParameterTypeAttribute), false);
+
+                ParameterTypeAttribute metadata;
+                if (attributes != null && attributes.Length > 0)
+                {
+                    metadata = attributes.Single();//make sure that there are no multiple attributes 
+                }
+                else
+                {
+                    metadata = new ParameterTypeAttribute();
+                }
+
+                if (!metadata.IgnoreField)
+                {
+                    object value = prop.GetValue(entity);
+                    dynamicParameters.Add("@" + prop.Name, value, null, metadata.Direction);
+                }
+            }
+
+            return dynamicParameters;
+        }
+    }
+}
---
+++ DapperStoredProcAttributes/ParameterTypeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+
+namespace PortalData.Utils
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    sealed class ParameterTypeAttribute : Attribute
+    {
+
+        //Overloaded constructors are done purposefully , changing it to ParameterTypeAttribute(ParameterDirection direction= ParameterDirection.Input, bool ignoreField=false ) makes to look the place where the attribute is used funny. more over , I do not want to recompile the target assemblies if the signature changes 
+
+        public ParameterTypeAttribute() : this(ParameterDirection.Input, false)
+        {
+
+        }
+
+        public ParameterTypeAttribute(bool ignoreField) : this(ParameterDirection.Input, ignoreField)
+        {
+
+        }
+
+        public ParameterTypeAttribute(ParameterDirection direction) : this(direction, false)
+        {
+
+        }
+
+        public ParameterTypeAttribute(ParameterDirection direction, bool ignoreField)
+        {
+            Direction = direction;
+            IgnoreField = ignoreField;
+
+        }
+
+        public ParameterDirection Direction { get; set; }
+        public bool IgnoreField { get; set; }
+    }
+}
