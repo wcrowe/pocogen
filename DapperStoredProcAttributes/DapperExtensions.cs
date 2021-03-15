cfd7822b2de8bd750d7fd49014a0618c92961382
IASADMIN\william.crowe <william.crowe@iasadmin.com>
2021-03-15 16:37:10 -0400

    Dapper Poco to Proc

--- DapperStoredProcAttributes/DapperExtensions.cs
+++ DapperStoredProcAttributes/DapperExtensions.cs
@@ -1,12 +1,9 @@
 ﻿using Dapper;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PortalData.Utils
 {
